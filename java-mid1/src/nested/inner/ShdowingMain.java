cdec759324078f3efbed518d8ca903b86d436e9c
Yongbb_13 <hvyongkim2@naver.com>
2024-04-18 11:48:35 +0900

    JAVA 중급 인프런 0418

---
+++ java-mid1/src/nested/inner/ShdowingMain.java
@@ -0,0 +1,29 @@
+package nested.inner;
+
+public class ShdowingMain {
+
+    public int value = 1;
+
+    class Inner {
+
+        public int value = 1;
+
+        void go (){
+            int value = 3;
+            System.out.println("value = " + value);
+            System.out.println("this.value = " + this.value);
+            System.out.println("ShdowingMain.value = " +
+                    ShdowingMain.this.value);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        ShdowingMain main = new ShdowingMain();
+        Inner inner = main.new Inner();
+        inner.go();
+
+    }
+
+}

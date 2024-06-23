492ea27c7e770a70a40a03ed14794af9d4d1a0b1
Yongbb_13 <hvyongkim2@naver.com>
2024-06-23 11:31:52 +0900

    checked, unchecked 예외 구분

---
+++ java-mid1/src/exception/basic/unchecked/Service.java
@@ -0,0 +1,21 @@
+package exception.basic.unchecked;
+
+public class Service {
+
+    Client client = new Client();
+
+    public void callCatch(){
+
+        try {
+        client.call();
+        }catch (MyUncheckedException e){
+            System.out.println("예외처리, message : " + e.getMessage());
+        }
+
+        System.out.println("정상로직");
+    }
+
+    public void callThrow(){
+        client.call();
+    }
+}
